7851325198805a84581a649a9a388d6697326886
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-09 14:15:07 -0500

    done app, need refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning the DB...'
+Restaurant.destroy_all
+puts 'DB is clean!'
+
+puts 'Creating 5 restaurants...'
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "Restaurant #{restaurant.id} was created"
+end
+
+puts 'All done!'
